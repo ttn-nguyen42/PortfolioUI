016c459769215d5b19c99b7a53c377d6cd9a7b9f
Trung Nguyên Trần <ttn.nguyen42@gmail.com>
2022-07-29 01:02:13 +0700

    added animation for links and project page bottom accordion

--- src/components/link/Link.tsx
+++ src/components/link/Link.tsx
@@ -1,16 +1,19 @@
+import Reveal from '../animate/Reveal';
 import { LinkPair } from '../points/Point';
 
 import s from './Link.module.scss';
 
 export default function Link({ link }: LinkComponentInterface) {
   return (
-    <div className={s['link-wrapper']}>
-      {'['}
-      <a href={link.link} target='_blank' rel='noreferrer'>
-        {link.name}
-      </a>
-      {']'}
-    </div>
+    <Reveal>
+      <div className={s['link-wrapper']}>
+        {'['}
+        <a href={link.link} target='_blank' rel='noreferrer'>
+          {link.name}
+        </a>
+        {']'}
+      </div>
+    </Reveal>
   );
 }
 
