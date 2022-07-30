0db05af51fae5617a5094264721de2c2003c2d80
Trung Nguyên Trần <ttn.nguyen42@gmail.com>
2022-07-30 18:41:54 +0700

    set pointer for info links, and footer links

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -8,11 +8,27 @@ export default function Footer() {
       <div className={s['wrapper']}>
         <section className={s['additional-links']}>
           <span className={s['visit']}>SEE THIS PROJECT ON:</span>
-          <div className={s['logo-text']}>
+          <div
+            className={s['logo-text']}
+            onClick={() => {
+              window.open(
+                'https://github.com/ttn-nguyen42/PortfolioUI',
+                '_blank'
+              );
+            }}
+          >
             <FontAwesomeIcon icon={faGithub} />
             <span className={s['text']}>{'PortfolioUI'}</span>
           </div>
-          <div className={s['logo-text']}>
+          <div
+            className={s['logo-text']}
+            onClick={() => {
+              window.open(
+                'https://github.com/ttn-nguyen42/PortfolioAPI',
+                '_blank'
+              );
+            }}
+          >
             <FontAwesomeIcon icon={faGithub} />
             <span className={s['text']}>{'PortfolioAPI'}</span>
           </div>
